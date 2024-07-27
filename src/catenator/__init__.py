bbfa6595cf35e02f68034573b2a9c83f56c2b0e0
Philip Orange <github@philiporange.com>
2024-07-27 20:35:00 +0100

    Remove ignored files from tree

--- src/catenator/__init__.py
+++ src/catenator/__init__.py
@@ -1,7 +1,7 @@
 from .catenator import Catenator
 
 __project__ = "catenator"
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __author__ = "Philip Orange"
 __email__ = "git@philiporange.com"
 __all__ = ["Catenator"]
