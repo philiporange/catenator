e0aa865ded6119aa22cfe67586a2f329b1090262
Philip Orange <git@philiporange.com>
2025-01-24 11:10:39 +0000

    Add cat.md to default ignore file

--- src/catenator/__init__.py
+++ src/catenator/__init__.py
@@ -1,7 +1,7 @@
 from .catenator import Catenator
 
 __project__ = "catenator"
-__version__ = "0.2.5"
+__version__ = "0.2.6"
 __author__ = "Philip Orange"
 __email__ = "git@philiporange.com"
 __description__ = "A Python tool for concatenating code."
